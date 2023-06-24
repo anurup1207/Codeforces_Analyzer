5ca544723cd26ac8b7ef24e6cdac8d3d74e47104
anurup1207 <anurup123aryan@gmail.com>
2023-06-24 15:08:52 +0530

    second commit

--- Backend/app.js
+++ Backend/app.js
@@ -3,15 +3,21 @@ async function user_status() {
     const api=`https://codeforces.com/api/user.status?handle=${user_name}&from=1`
     let response=await fetch(api)
     response = await response.json()
-    // console.log(response)
-    // let response_length=Object.keys(response).length
-    // console.log(response_length)
-    // for(let i=0;i<response.size)
+    
     var result = [];
     var result = response["result"];
-    // console.log(result[0]["problem"]);
+    
     var mp_for_verdict = new Map();
+    var mp_for_language= new Map();
     for(var i in result){
+        // <<<=========== language ==============>>>
+
+        if (mp_for_language.has(result[i]["programmingLanguage"]))
+        mp_for_language.set(result[i]["programmingLanguage"], mp_for_language.get(result[i]["programmingLanguage"]) + 1)
+    else
+        mp_for_language.set(result[i]["programmingLanguage"], 1)
+
+       // <<<=========== language ==============>>>
 
 
         // <<<=========== verdict ==============>>>
@@ -24,22 +30,58 @@ async function user_status() {
        // <<<=========== verdict ==============>>>
         
     }
+
+
+
+
+
+     // <<<=========== language ==============>>>
+
+     const languages=[];
+     for(var i in result){
+         if (mp_for_language.get(result[i]["programmingLanguage"]) != -1) {
+             let tag1=result[i]["programmingLanguage"];
+             let value=mp_for_language.get(result[i]["programmingLanguage"]);
+             languages.push({
+                
+                 [tag1]:value,
+             })
+            
+             mp_for_language.set(result[i]["programmingLanguage"], -1);
+         }
+     }
+     for(let i in languages){
+         console.log(languages[i]);
+     }
+     // <<<=========== language ==============>>>
+
+
+
+
+
+
+
+
+
+
+
+    // <<<=========== verdict ==============>>>
     const verdict=[];
     for(var i in result){
         if (mp_for_verdict.get(result[i]["verdict"]) != -1) {
             let tag1=result[i]["verdict"];
             let value=mp_for_verdict.get(result[i]["verdict"]);
             verdict.push({
-                id:tag1,
-                values:value,
+                [tag1]:value,
             })
            
             mp_for_verdict.set(result[i]["verdict"], -1);
         }
     }
-    for(let i in verdict){
-        console.log(verdict[i]);
-    }
+    // for(let i in verdict){
+    //     console.log(verdict[i]);
+    // }
+    // <<<=========== verdict ==============>>>
 }
 
 user_status()
