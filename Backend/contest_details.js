4f5fad9cdd07413829df5cb4ea602b6a3e6455af
anurup1207 <anurup123aryan@gmail.com>
2023-06-27 02:16:29 +0530

    contest_details

--- Backend/contest_details.js
+++ Backend/contest_details.js
@@ -13,12 +13,32 @@ let current_rating=0;
 let max_up=0;
 let max_down=0;
 let worst_rank=0;
+let contestid_best_rank;
+let contestid_worst_rank;
+let contestid_max_up;
+let contestid_max_low=0;
 for(let i in result){
     let rating_change= result[i]["newRating"]-result[i]["oldRating"]
-    max_up= Math.max(rating_change, max_up);
-    max_down= Math.min(rating_change,max_down);
-    best_rank= Math.min(best_rank,result[i]["rank"]);
-    worst_rank=Math.max(worst_rank,result[i]["rank"])
+    
+    if(max_up<rating_change){
+        max_up=rating_change;
+        contestid_max_up=result[i]["contestId"];
+    }
+    
+    if(max_down>rating_change){
+        max_down=rating_change;
+        contestid_max_low=result[i]["contestId"];
+    }
+    
+    if(best_rank>result[i]["rank"]){
+        best_rank=result[i]["rank"];
+        contestid_best_rank=result[i]["contestId"];
+    }
+    
+    if(worst_rank<result[i]["rank"]){
+        worst_rank=result[i]["rank"];
+        contestid_worst_rank=result[i]["contestId"]
+    }
     max_rating= Math.max(max_rating,result[i]["newRating"])
     current_rating=result[i]["newRating"]
 
@@ -38,15 +58,19 @@ contest_details.push({
 })
 contest_details.push({
     "best rank" : best_rank,
+    "link ":`https://codeforces.com/contest/${contestid_best_rank}`, 
 })
 contest_details.push({
     "worst rank" : worst_rank,
+    "link ": `https://codeforces.com/contest/${contestid_worst_rank}`,
 })
 contest_details.push({
     "max up" : max_up,
+    "link " : `https://codeforces.com/contest/${contestid_max_up}`,
 })
 contest_details.push({
     "max down": max_down,
+    "link ": `https://codeforces.com/contest/${contestid_max_low}`,
 })
 
 for(let i in contest_details){
