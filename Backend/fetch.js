202657465763fbd7ce8c23ec9f2b81447650f768
anurup1207 <anurup123aryan@gmail.com>
2023-06-27 01:34:13 +0530

    contest_details

--- Backend/fetch.js
+++ Backend/fetch.js
@@ -6,5 +6,13 @@ async function user_status() {
     return response;
 }    
 
+async function user_contest_details(){
+    const user_name="anurup123aryan"
+    const api=`https://codeforces.com/api/user.rating?handle=${user_name}`
+    let response=await fetch(api)
+    response = await response.json();
+    return response;
+}
 
-module.exports={user_status}
+
+module.exports={user_status,user_contest_details}
