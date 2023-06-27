5f28f352ea68fa5c3b932e8df15b81fe9aa10538
anurup1207 <anurup123aryan@gmail.com>
2023-06-27 16:00:49 +0530

    seventh commit

--- Backend/fetch.js
+++ Backend/fetch.js
@@ -1,9 +1,16 @@
 async function user_status() {
-    const user_name="anurup123aryan"
-    const api=`https://codeforces.com/api/user.status?handle=${user_name}&from=1`
-    let response=await fetch(api)
-    response = await response.json();
-    return response;
+    // try{
+        const user_name="anurup123aryan"
+        const api=`https://codeforces.com/api/user.status?handle=${user_name}&from=1`
+        let response=await fetch(api)
+        response = await response.json();
+        return response;
+    // }catch(e){
+    //     console.log("Error detected");
+    //     return;
+
+    // }
+   
 }    
 
 async function user_contest_details(){
