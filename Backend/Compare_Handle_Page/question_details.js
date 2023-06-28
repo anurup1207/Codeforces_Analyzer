d16d687211f61d7a9f50b448c9c6f6f3506ca987
anurup1207 <anurup123aryan@gmail.com>
2023-06-28 16:52:57 +0530

    question_details

--- Backend/Compare_Handle_Page/question_details.js
+++ Backend/Compare_Handle_Page/question_details.js
@@ -2,92 +2,86 @@
 const res = require("./fetch");
 
 function max_current_rating(result) {
-    result.reverse();
-    let mp_for_tried_questions = new Map();
-    let mp_for_solved_questions = new Map();
-    let mp_for_attempt_to_each_questions= new Map();
-    let mp_for_ifOk= new Map();
-  
-    for (let i in result) {
-      let contestidindex = result[i]["problem"]["contestId"]
-      contestidindex += "-"
-      contestidindex += result[i]["problem"]["index"]
-      // console.log(contestidindex)
-      let ifOK= result[i]["verdict"]
-      if(mp_for_ifOk.has(contestidindex)==false){
-          
-  
-              if (mp_for_attempt_to_each_questions.has(contestidindex))
-                  mp_for_attempt_to_each_questions.set(contestidindex, mp_for_attempt_to_each_questions.get(contestidindex) + 1);
-              else
-                  mp_for_attempt_to_each_questions.set(contestidindex, 1);
-               
-              // mp_for_solved_questions.set(contestidindex,1)
-         
-          if(ifOK=='OK'){
-              mp_for_ifOk.set(contestidindex,1);
-              mp_for_solved_questions.set(contestidindex,1);
-          }
-      }
-      mp_for_tried_questions.set(contestidindex,1);
-  
-     
-  
-     
-    }
-  
-  
-    let max_attempt=0;
-    let max_question_name;
-    let attempt_one=0;
-    let contestid;
-    let index;
-    const attempt_to_each_question = [];
-    for (let i in result) {
-      let contestidindex = result[i]["problem"]["contestId"]
-      contestidindex += "-"
-      contestidindex += result[i]["problem"]["index"]
-      if (mp_for_attempt_to_each_questions.get(contestidindex) != -1) {
-        let tag1 = contestidindex;
-        let value = mp_for_attempt_to_each_questions.get(contestidindex);
-        if(value==1){
-          attempt_one++;
-        }
-        if(value > max_attempt){
-          max_attempt=value
-          max_question_name=tag1
-          contestid=result[i]["problem"]["contestId"]
-          index=result[i]["problem"]["index"]
-        }
-        attempt_to_each_question.push({
-          [tag1]: value,
-        });
-        mp_for_attempt_to_each_questions.set(contestidindex, -1);
+  result.reverse();
+  let mp_for_tried_questions = new Map();
+  let mp_for_solved_questions = new Map();
+  let mp_for_attempt_to_each_questions = new Map();
+  let mp_for_ifOk = new Map();
+
+  for (let i in result) {
+    let contestidindex = result[i]["problem"]["contestId"];
+    contestidindex += "-";
+    contestidindex += result[i]["problem"]["index"];
+    // console.log(contestidindex)
+    let ifOK = result[i]["verdict"];
+    if (mp_for_ifOk.has(contestidindex) == false) {
+      if (mp_for_attempt_to_each_questions.has(contestidindex))
+        mp_for_attempt_to_each_questions.set(
+          contestidindex,
+          mp_for_attempt_to_each_questions.get(contestidindex) + 1
+        );
+      else mp_for_attempt_to_each_questions.set(contestidindex, 1);
+
+      // mp_for_solved_questions.set(contestidindex,1)
+
+      if (ifOK == "OK") {
+        mp_for_ifOk.set(contestidindex, 1);
+        mp_for_solved_questions.set(contestidindex, 1);
       }
     }
-    let percentage=attempt_one/mp_for_solved_questions.size;
-    percentage=percentage*100;
-    percentage=percentage.toFixed(2);
-  
-    let average1=result.length/mp_for_solved_questions.size;
-    let average2=average1.toFixed(2);
-    
-      let question_details={
-        "tried": mp_for_tried_questions.size,
-        "solved": mp_for_solved_questions.size,
-        "unsolved":mp_for_tried_questions.size- mp_for_solved_questions.size,
-        "max-attempt":{
-          "max-attempt" : max_attempt,
-          "problem-name" : max_question_name,
-          "link" : `https://codeforces.com/contest/${contestid}/problem/${index}`,
-        },
-        "attempt-one":{
-          "Solved with one submission" : attempt_one,
-           "percentage ": percentage,
-        },
-        "average " : average2,
-  
+    mp_for_tried_questions.set(contestidindex, 1);
+  }
+
+  let max_attempt = 0;
+  let max_question_name;
+  let attempt_one = 0;
+  let contestid;
+  let index;
+  const attempt_to_each_question = [];
+  for (let i in result) {
+    let contestidindex = result[i]["problem"]["contestId"];
+    contestidindex += "-";
+    contestidindex += result[i]["problem"]["index"];
+    if (mp_for_attempt_to_each_questions.get(contestidindex) != -1) {
+      let tag1 = contestidindex;
+      let value = mp_for_attempt_to_each_questions.get(contestidindex);
+      if (value == 1) {
+        attempt_one++;
       }
+      if (value > max_attempt) {
+        max_attempt = value;
+        max_question_name = tag1;
+        contestid = result[i]["problem"]["contestId"];
+        index = result[i]["problem"]["index"];
+      }
+      attempt_to_each_question.push({
+        [tag1]: value,
+      });
+      mp_for_attempt_to_each_questions.set(contestidindex, -1);
+    }
+  }
+  let percentage = attempt_one / mp_for_solved_questions.size;
+  percentage = percentage * 100;
+  percentage = percentage.toFixed(2);
+
+  let average1 = result.length / mp_for_solved_questions.size;
+  let average2 = average1.toFixed(2);
+
+  let question_details = {
+    tried: mp_for_tried_questions.size,
+    solved: mp_for_solved_questions.size,
+    unsolved: mp_for_tried_questions.size - mp_for_solved_questions.size,
+    "max-attempt": {
+      "max-attempt": max_attempt,
+      "problem-name": max_question_name,
+      link: `https://codeforces.com/contest/${contestid}/problem/${index}`,
+    },
+    "attempt-one": {
+      "Solved with one submission": attempt_one,
+      "percentage ": percentage,
+    },
+    "average ": average2,
+  };
 
   return question_details;
 }
