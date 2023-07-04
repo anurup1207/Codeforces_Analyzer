acedd9ef6081e8486192b4cfb6bcf190507f84f2
Harsh Yadav <harshyadav1393@gmail.com>
2023-07-04 20:45:09 +0530

    Layout completed

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -4,20 +4,21 @@ const Navbar = () => {
   return (
     <div>
       {/* <img src="Codeforces.png" className="mx-2" alt="" /> */}
-      <div className="text-center my-3 navbar">
-        <h3>
+      <div className="my-3">
+        <h3 className="text-center cf">
+          <img src="Codeforces.png" alt="" className=" mx-1" />
           <blue style={{ color: "#4082CE" }}>CF</blue>
           <brown style={{ color: "#C44739" }}>analyst</brown>
         </h3>
       </div>
 
-      <div className="section text-center">
-        <h5 className="row1">Analyze</h5>
-        <h5 className="row1">Compare</h5>
-        <h5 className="row1">Virtual Rating</h5>
-        <h5 className="row1">Services</h5>
-        <h5 className="row1">Rank Predictor</h5>
-        <h5 className="row1">Feedback</h5>
+      <div className="section my-5 ">
+        <div className="mx-2"><img src="statistic 1.png" alt="" /><h5 className="my-4 mx-1 features">Analyze</h5></div>
+        <div className="mx-2"><img src="transfer.png" alt="" /><h5 className="my-4 mx-1 features">Compare</h5></div>
+        <div className="mx-2"><img src="Menu icon.png" alt="" /><h5 className="my-4 mx-1 features">Virtual Rating</h5></div>
+        <div className="mx-2"><img src="gift.png" alt="" /><h5 className="my-4 mx-1 features">Services</h5></div>
+        <div className="mx-2"><img src="rank.png" alt="" /><h5 className="my-4 mx-1 features">Rank Predictor</h5></div>
+        <div className="mx-2"><img src="feedback.png" alt="" /><h5 className="my-4 mx-1 features">Feedback</h5></div>
       </div>
 
     </div>
