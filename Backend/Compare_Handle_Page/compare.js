8778c1efc4b1e057577d823155f8be87ad683774
anurup1207 <anurup123aryan@gmail.com>
2023-07-10 21:27:43 +0530

    level

--- Backend/Compare_Handle_Page/compare.js
+++ Backend/Compare_Handle_Page/compare.js
@@ -1,8 +1,9 @@
 const fetch1 = require('../fetch');
+const { level } = require('./level');
 const { user_question_details } = require('./question_details');
-const question_rating  = require('./question_rating');
-const ta= require('./tags');
-const t_not_solved = require('./tried_not_solved');
+const {question_rating}  = require('./question_rating');
+const {tags}= require('./tags');
+const { tried_not_solved} = require('./tried_not_solved');
 
 
 
@@ -24,18 +25,21 @@ async function compare(user_name1,user_name2){
     final_self["status"]="OK"
     final_self["result"]={}
 
-    let tried_not_solved=await t_not_solved.tried_not_solved(result_user_name1_user_status["result"],result_user_name2_user_status["result"]);
-    final_self["result"]["tried_not_solved"]=tried_not_solved;
+    let tried_not_solved1=await tried_not_solved(result_user_name1_user_status["result"],result_user_name2_user_status["result"]);
+    final_self["result"]["tried_not_solved"]=tried_not_solved1;
 
-    let tag= await ta.tags(result_user_name1_user_status["result"],result_user_name2_user_status["result"]);
+    let tag= await tags(result_user_name1_user_status["result"],result_user_name2_user_status["result"]);
     final_self["result"]["tags"]=tag;
 
-    let q_rating= await question_rating.question_rating(result_user_name1_user_status["result"],result_user_name2_user_status["result"]);
+    let q_rating= await question_rating(result_user_name1_user_status["result"],result_user_name2_user_status["result"]);
     final_self["result"]["question_rating"]=q_rating;
 
     let q_details= await user_question_details(result_user_name1_user_status["result"],result_user_name2_user_status["result"]);
     final_self["result"]["question_details"]=q_details;
-    
+
+    let level1= await level(result_user_name1_user_status["result"],result_user_name2_user_status["result"]);
+    final_self["result"]["level"]=level1;
+
     console.log(final_self);
     return final_self;
 
